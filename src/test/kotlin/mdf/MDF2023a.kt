95dabb9c898f805d42e6e4577df95e2ecf135637
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-03-08 23:38:22 +0100

    Add mdf 2023a

---
+++ src/test/kotlin/mdf/MDF2023a.kt
@@ -0,0 +1,50 @@
+package mdf
+
+import org.junit.Test
+
+class MDF2023a : BaseTest("MDF2023a") {
+    // println(p(lines))
+
+    @Test
+    fun test1() = test(0, ::p1)
+
+    @Test
+    fun test2() = test(0, ::p2)
+
+    @Test
+    fun test3() = test(0, ::p3)
+
+
+    private fun p1(lines: List<String>): Any {
+        return lines
+    }
+
+    private fun p2(lines: List<String>): Any {
+        return lines
+    }
+
+    private fun p3(lines: List<String>): Any {
+        return lines
+    }
+
+    @Test
+    fun test4() = test(0, ::p4)
+
+    @Test
+    fun test5() = test(0, ::p5)
+
+    @Test
+    fun test6() = test(0, ::p6)
+
+    private fun p4(lines: List<String>): Any {
+        return lines
+    }
+
+    private fun p5(lines: List<String>): Any {
+        return lines
+    }
+
+    private fun p6(lines: List<String>): Any {
+        return lines
+    }
+}
