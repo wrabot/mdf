b573dca57e3d8cf2fe82d09b1dc5c52d714da3ed
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-10-18 20:30:02 +0200

    Update version tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("wrabot.competitive:CompetitiveTools:0.35")
+    testImplementation("wrabot.competitive:CompetitiveTools:0.36")
     // to test new CompetitiveTools
     //testImplementation("wrabot.competitive:CompetitiveTools") { version { branch = "main" } }
     testImplementation("junit:junit:4.13.2")
