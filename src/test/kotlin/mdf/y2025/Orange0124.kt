455207758110ef52a1b45ce4be347bf0ccb58252
Wilfrid Rabot <wilfrid.rabot@orange.com>
2025-01-24 12:00:36 +0100

    Add orange 24/1/2025

---
+++ src/test/kotlin/mdf/y2025/Orange0124.kt
@@ -0,0 +1,31 @@
+package mdf.y2025
+
+import mdf.BaseTest
+import org.junit.Test
+
+class Orange0124 : BaseTest() {
+    @Test
+    fun test1() = test(1, ::p1)
+
+    @Test
+    fun test2() = test(0, ::p2)
+
+    @Test
+    fun test3() = test(0, ::p3)
+
+    @Test
+    fun test4() = test(0, ::p4)
+
+    @Test
+    fun test5() = test(0, ::p5)
+
+    @Test
+    fun test6() = test(0, ::p6)
+
+    private fun p1() {}
+    private fun p2() {}
+    private fun p3() {}
+    private fun p4() {}
+    private fun p5() {}
+    private fun p6() {}
+}
